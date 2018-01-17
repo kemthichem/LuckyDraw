43e58ba005ae465af25d925aa767f168f9893f6c
Than Huynh <thanhv92@gmail.com>
2018-01-17 21:51:51 +0700

    Add model

---
+++ implement/LuckyDraw/LuckyDraw/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyDraw
+{
+    class Person
+    {
+        private int mId;
+        private string mName;
+        private string mInfo;
+        private string mAwardName;
+
+        public Person()
+        {
+
+        }
+    }
+}
